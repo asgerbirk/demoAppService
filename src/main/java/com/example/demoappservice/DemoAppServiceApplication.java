c1af51d18c0551a2a9a970ec01e21137766121da
asgerbirk <asger.birk@gmail.com>
2022-09-05 21:17:10 +0200

    first

---
+++ src/main/java/com/example/demoappservice/DemoAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoappservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAppServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoappservice/DemoAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoappservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
